883fa14ec3372cd327eb6c23693bb0bbd07038c1
Olga Karataeva <olgakarataeva1998@mail.ru>
2017-04-13 10:53:13 +0700

    1

---
+++ Notepad/Notepad.ViewModel/NotepadViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Notepad.Model;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace Notepad.ViewModel
+{
+    public class NotepadViewModel //:INotifyPropertyChanged:
+    {
+
+    }
+}
